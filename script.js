642a77092c2c8f549d700c5f6e117efdf732b5f9
saurabhcses <157964303+saurabhcses@users.noreply.github.com>
2025-08-02 12:53:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function arrange(str){
+	      return name.replace(/^(a |an |the )/i, '').trim();
+    }
+
+    const sortedBands = bands.sort((a, b) => {
+      return arrange(a).localeCompare(arrange(b));
+    });
