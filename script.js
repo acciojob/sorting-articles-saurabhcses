cc049231223ef178992bab8b0c165a3d1e7323be
saurabhcses <157964303+saurabhcses@users.noreply.github.com>
2025-08-06 21:49:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-//your JS code here. If required.
-function arrange(str){
-	      return name.replace(/^(a |an |the )/i, '').trim();
-    }
+function arrange(str) {
+  return str.replace(/^(a |an |the )/i, '').trim();
+}
 
-    const sortedBands = bands.sort((a, b) => {
-      return arrange(a).localeCompare(arrange(b));
-    });
+const bands = [
+  'The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean',
+  'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans',
+  'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'
+];
+
+const sortedBands = bands.sort((a, b) => {
+  return arrange(a).localeCompare(arrange(b));
+});
